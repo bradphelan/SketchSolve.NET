c6902c4ae0b17eb973df8cdb36eac7d5e5da96f2
Brad Phelan <bradphelan@xtargets.com>
2013-05-30 20:09:39 +0200

    Add Should().BeApproximately(…)

--- SketchSolveC#.NET.Spec/Test.cs
+++ SketchSolveC#.NET.Spec/Test.cs
@@ -3,9 +3,18 @@ using NUnit.Framework;
 using FluentAssertions;
 using SketchSolve;
 using System.Linq;
+using FluentAssertions.Numeric;
 
 namespace SketchSolve.Spec
 {
+   public static class FluentExtensionsX {
+        public static AndConstraint<NumericAssertions<double>> BeApproximately
+            (this NumericAssertions<double> This, double val, double eps)
+        {
+            return This.BeInRange (val - eps/2, val + eps / 2);
+        }
+   }
+
   [TestFixture()]
   public class Solver
   {
@@ -20,7 +29,7 @@ namespace SketchSolve.Spec
         , line.IsHorizontal()
         );
 
-      line.p1.y.Value.Should ().BeInRange (line.p2.y.Value-0.001, line.p2.y.Value+0.001); 
+        line.p1.y.Value.Should ().BeApproximately (line.p2.y.Value, 0.001);
     }
 
     [Test()]
@@ -34,7 +43,7 @@ namespace SketchSolve.Spec
         , line.IsVertical()
         );
 
-      line.p1.x.Value.Should ().BeInRange (line.p2.x.Value-0.001, line.p2.x.Value+0.001); 
+        line.p1.x.Value.Should ().BeApproximately (line.p2.x.Value, 0.001);
     }
   }
 }
