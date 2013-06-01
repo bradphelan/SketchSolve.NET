c943c5b87fc0f22b66b4b9172a4e0ef5a5c575f5
Brad Phelan <bradphelan@xtargets.com>
2013-06-01 20:40:54 +0200

    Simplify usage of the solver

--- SketchSolveC#/Parameter.cs
+++ SketchSolveC#/Parameter.cs
@@ -9,6 +9,8 @@ namespace SketchSolve
     public class Parameter
     {
         public double Value = 0;
+        public double Max = 1000;
+        public double Min = -1000;
         // true if the parameter is free to be adjusted by the
         // solver
         public bool free;
